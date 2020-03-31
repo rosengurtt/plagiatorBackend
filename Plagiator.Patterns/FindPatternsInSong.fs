9d008ba0b89d19cb2bf62014144c869301b383e8
rosengurtt <rosengurtt@yahoo.com>
2020-03-31 21:33:16 +0100

    migrated some c# to f#

--- Plagiator.Patterns/FindPatternsInSong.fs
+++ Plagiator.Patterns/FindPatternsInSong.fs
@@ -4,6 +4,44 @@ open Plagiator.Models
 open System.Collections.Generic
 open System
 open System.Linq
+open Plagiator.Models.Entities
 
-//module public PatternsInSong =
+module public PatternsInSong =
 
+    let IntersectionComparedToDuration (n1: Note, n2: Note): double =
+        let intersection = Math.Min(n1.EndSinceBeginningOfSongInTicks, n2.EndSinceBeginningOfSongInTicks) -
+            Math.Max(n1.StartSinceBeginningOfSongInTicks, n2.StartSinceBeginningOfSongInTicks)
+        let  averageDuration: float = float (n1.DurationInTicks + n2.DurationInTicks) / (float 2)
+        (float) intersection / averageDuration
+
+
+
+    let RemoveBassNotes (notes: List<Note>): List<Note> =
+        let sortedNotes = notes |> Seq.sortBy(fun x -> x.StartSinceBeginningOfSongInTicks, x.Pitch) |>  Seq.toList
+        let mutable i = 0
+        let fsharpIsShit = new  List<Note> ()
+        while  i < sortedNotes.Count() - 1 do
+            let mutable j = 0
+            while i+j < sortedNotes.Count() && 
+                sortedNotes.[i+j].StartSinceBeginningOfSongInTicks < sortedNotes.[i].EndSinceBeginningOfSongInTicks &&
+                IntersectionComparedToDuration(sortedNotes.[i + j], sortedNotes.[j]) > 0.7 do
+                j <- j + 1
+            if i + j < sortedNotes.Count() then
+                fsharpIsShit.Add( sortedNotes.[i + j - 1])
+            else
+                ()
+            if j = 0 then
+                i <- i + 1
+            else
+                i <- i + j 
+        fsharpIsShit
+
+
+    type melody = class
+        val Notes: List<Note>
+
+        new (notes:List<Note>) as this =
+            { Notes = notes }
+
+
+    end
